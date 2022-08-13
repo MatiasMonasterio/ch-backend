c0ed837e5ac2b31dae9285f20e208ec14ac27606
MatiasMonasterio <matias_monasterio@outlook.com>
2022-08-13 12:22:51 -0300

    docs: add pug project
    Signed-off-by: MatiasMonasterio <matias_monasterio@outlook.com>

---
+++ class-05/deliverable/pug/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const pug = require("pug");
+const Container = require("./models/Container");
+
+const app = express();
+const productsContainer = new Container("products.txt");
+
+app.use(express.urlencoded({ extended: true }));
+
+// app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.locals.basedir = app.get("views");
+
+app.get("/", (_req, res) => {
+  res.render("index");
+});
+
+app.post("/products", async (req, res) => {
+  const { name: title, price, image: thumbnail } = req.body;
+  await productsContainer.save({ title, price, thumbnail });
+
+  res.redirect(303, "/");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await productsContainer.getAll();
+
+  res.render("products", {
+    products: products,
+    productsExist: !!products.lenght,
+  });
+});
+
+const server = app.listen(3000, () => {
+  console.log(`Server init on port ${3000}`);
+});
+
+server.on("error", (error) => {
+  console.error(`Server Error: ${error}`);
+});
